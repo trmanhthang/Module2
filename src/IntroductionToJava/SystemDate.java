f4307f309cb00ee68e64be0b685a44e4a72030cd
trmanhthang <bigzeen861998@gmail.com>
2022-12-02 16:22:29 +0700

    commit

---
+++ src/IntroductionToJava/SystemDate.java
@@ -0,0 +1,11 @@
+package IntroductionToJava;
+
+import java.util.Date;
+
+public class SystemDate {
+    public static void main(String[] args) {
+        Date now = new Date();
+
+        System.out.println("Now is: " + now);
+    }
+}
