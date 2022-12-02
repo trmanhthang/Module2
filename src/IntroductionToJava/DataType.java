10304dea774e17910c62c709dc3964f4c9565348
trmanhthang <bigzeen861998@gmail.com>
2022-12-02 16:33:54 +0700

    commit

---
+++ src/IntroductionToJava/DataType.java
@@ -0,0 +1,24 @@
+package IntroductionToJava;
+
+public class DataType {
+    public static void main(String[] args) {
+        int i = 10;
+        System.out.println("i = " + i);
+
+        float f = 20.5f;
+        System.out.println("f = " + f);
+
+        double d = 20.5;
+        System.out.println("d = " + d);
+
+        boolean b = true;
+
+        System.out.println("b = " + b);
+
+        char c = 'a';
+        System.out.println("c = " + c);
+
+        String s = "Hà Nội";
+        System.out.println("s = " + s);
+    }
+}
