69aa76a39d271a227e009d9c4961e59ca7676ae3
trmanhthang <bigzeen861998@gmail.com>
2022-12-09 08:25:32 +0700

    upload file

---
+++ src/ArrJava/TestCircle/Circle.java
@@ -0,0 +1,21 @@
+package ArrJava.TestCircle;
+
+public class Circle {
+    private double radius = 1.0;
+    private String color = "red";
+
+    public Circle() {
+    }
+
+    public Circle(double radius) {
+        this.radius = radius;
+    }
+
+    public double getRadius() {
+        return radius;
+    }
+
+    public double getArea() {
+        return 3.14 * radius * 2;
+    }
+}
