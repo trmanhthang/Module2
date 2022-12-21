48421f10b2cc1f2ba5bbe0f5611c5c0d6564356e
trmanhthang <bigzeen861998@gmail.com>
2022-12-21 15:30:25 +0700

    upload file

--- src/Demo/BubbleSort.java
+++ src/Demo/BubbleSort.java
@@ -2,18 +2,21 @@ package Demo;
 
 public class BubbleSort {
     static void bubbleSort(int[] array) {
-        for (int i = 0; i < array.length - 1; i++) {
+        boolean check = true;
+        for (int i = 0; i < array.length - 1 && check; i++) {
             for (int j = array.length - 1; j > i; j--) {
+                check = false;
                 if (array[j] < array[j - 1]) {
                     int temp = array[j];
                     array[j] = array[j - 1];
                     array[j - 1] = temp;
+                    check = true;
                 }
             }
         }
         System.out.println("Sắp xếp nổi bọt: ");
         for (int i = 0; i < array.length; i++) {
-            System.out.println(array[i]);
+            System.out.print(array[i] + ", ");
         }
     }
 }
