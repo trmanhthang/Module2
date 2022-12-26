96ce056c785b9746ec91f0fd68cfad825e39418d
trmanhthang <bigzeen861998@gmail.com>
2022-12-26 16:07:37 +0700

    upload file

---
+++ src/DemoTestIO/IOBinary/Main.java
@@ -0,0 +1,50 @@
+package DemoTestIO.IOBinary;
+
+import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) {
+        List<Student> students = new ArrayList<>();
+        students.add(new Student(1, "Vũ Kiều Anh", "Hà Nội"));
+        students.add(new Student(2, "Nguyễn Minh Quân", "Hà Nội"));
+        students.add(new Student(3,"Đặng Huy Hoà", "Đà Nẵng"));
+        students.add(new Student(4, "Nguyễn Khánh Tùng", "Hà Nội"));
+        students.add(new Student(5, "Nguyễn Khắc Nhật", "Hà Nội"));
+        writeToFile("D:\\CG Java\\Module2\\src\\Text\\Student.txt", students);
+        List<Student> studentDataFromfile = readDataFromFile("D:\\CG Java\\Module2\\src\\Text\\Student.txt");
+        for (Student student : studentDataFromfile) {
+            System.out.println(student);
+        }
+    }
+
+//    Phương thức ghi vào file
+    public static void writeToFile(String path, List<Student> students) {
+        try {
+            FileOutputStream fos = new FileOutputStream(path);
+            ObjectOutputStream oos = new ObjectOutputStream(fos);
+            oos.writeObject(students);
+            oos.close();
+            fos.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+//    Đọc dữ liệu file
+    public static List<Student> readDataFromFile(String path) {
+        List<Student> students = new ArrayList<>();
+        try {
+            FileInputStream fis = new FileInputStream(path);
+            ObjectInputStream ois = new ObjectInputStream(fis);
+            students = (List<Student>) ois.readObject();
+            fis.close();
+            ois.close();
+        } catch (IOException | ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+        return students;
+    }
+
+}
