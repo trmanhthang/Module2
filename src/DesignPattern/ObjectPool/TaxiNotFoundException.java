fa72d4f9ed5ed2f59a9a657f7ab8a4b19da5cd1a
trmanhthang <bigzeen861998@gmail.com>
2022-12-30 09:31:18 +0700

    upload file

---
+++ src/DesignPattern/ObjectPool/TaxiNotFoundException.java
@@ -0,0 +1,7 @@
+package DesignPattern.ObjectPool;
+
+public class TaxiNotFoundException extends RuntimeException{
+    public TaxiNotFoundException(String message) {
+        System.out.println(message);
+    }
+}
